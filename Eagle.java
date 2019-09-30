a61f55e4674ed7301247179f9612937ec8333706
Sandra Roques <roquesandra@hotmail.fr>
2019-09-30 17:24:36 +0200

    FlyingEagle

--- Eagle.java
+++ Eagle.java
@@ -1,14 +1,16 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
 
+    //Constructeur
     public Eagle(String name) {
         super(name);
         this.flying = false;
         this.altitude = 0;
     }
 
+    //Getter
     public int getAltitude() {
         return altitude;
     }
@@ -17,8 +19,50 @@ public class Eagle extends Bird {
         return flying;
     }
 
+    //Méthode classe mère
     @Override
     public String sing() {
         return "Screech!";
+    }
+
+    //Implémentation des méthodes de l'interface Fly
+    @Override
+    public void takeOff(){
+        if (this.altitude == 0 && !this.flying) {
+            this.flying = true;
+            System.out.println(this.getName() + " take of in the sky !");
+        }
+        else {
+            System.out.println(this.getName() + " is already flying.");
+        }
+    }
+
+    @Override
+    public int ascend(int meters) {
+        if (this.flying) {
+            this.altitude += meters;
+            System.out.println(this.getName() + " fies upward, altitude : " + this.getAltitude() + ".");
+        }
+        return this.altitude;
+    }
+
+    @Override
+    public int descend(int meters) {
+        if (this.flying) {
+            this.altitude = Math.max(this.altitude - meters, 0);
+            System.out.println(this.getName() + " flies downward, altitude : " + this.getAltitude() + ".");
+        }
+        return this.altitude;
+    }
+
+    @Override
+    public void land() {
+        if (this.altitude <= 1 && this.flying) {
+            this.flying = false;
+            System.out.println(this.getName() + " lands on the ground.");
+        }
+        else {
+            System.out.println(this.getName() + " can't land, he has to get closer to the ground !");
+        }
     }
 }
